b875ec9a784ee80c4f8f65547f78c991c5d8e98e
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-06-17 18:08:36 -0300

    Update index.php

--- aula10/index.php
+++ aula10/index.php
@@ -1,5 +1,15 @@
 <?php 
-echo "<h1>HTML</h1>";
+echo "declarar contantes" . PHP_EOL;
+define('VALOR', 10); //letras maiusculas
+echo VALOR;
+echo "<br>";
+echo "<--------------------->";
+echo "<br>";
 ?>
 
-<h1>html</h1>
+<?php
+echo "constantes do php" . "<br>";
+echo PHP_VERSION . "<br>";
+echo "este codigo esta na linha " . __LINE__ . " (constante dinamica)";
+?>
+
