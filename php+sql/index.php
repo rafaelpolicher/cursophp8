fb3ede1cbb7ae3a001c78b373a45529070871f1b
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-07-26 19:48:22 -0300

    mysqli e pdo

--- php+sql/index.php
+++ php+sql/index.php
@@ -2,4 +2,11 @@
 cliente -> request servidor web <-> base de dados <br>
 resultados: nada/ valor unico/ arr associativo/ array de objetos <br>
 
-<h2></h2>
+<h2>MySQLi x PDO</h2><br>
+<b>MySQL improved</b> <br>
+vantagens: procedural ou orientada a objetos/ bom desempenho/ sintaxe simples <br>
+desvantagens: so permite connexao com mysql/ queries parametrizadas nao tem parametros nomeados <br>
+<b>php Data objects</b>
+vantagens: orientada a objetps/ tem parametros nomeados/ permite conexao com 12 tipos de base de dados <br>
+desvantagens: nao é tao performativo <br>
+mais recente e tendencia maior
